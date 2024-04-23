8dbf225788b80fd1ba395034d4463944b48a1d88
HagiriMigita <starlight8620@gmail.com>
2024-04-23 15:38:10 +0900

    実際の画像

--- Amazon Rekognition.py
+++ Amazon Rekognition.py
@@ -1,60 +1,25 @@
-import cv2
-import numpy as np
 import boto3
 
-# スケールや色などの設定
-scale_factor = .15
-green = (0,255,0)
-red = (0,0,255)
-frame_thickness = 2
-cap = cv2.VideoCapture(0)
-rekognition = boto3.client('rekognition')
+with open('face_image.jpg', 'rb') as image_file:
+    image = image_file.read()
 
-# フォントサイズ
-fontscale = 1.0
-# フォント色 (B, G, R)
-color = (0, 120, 238)
-# フォント
-fontface = cv2.FONT_HERSHEY_DUPLEX
+client = boto3.client('rekognition')
 
-# q を押すまでループします。
-while(True):
+response = client.detect_faces(
+    Image={
+        'Bytes': image
+    },
+    Attributes=['ALL']
+)
 
-    # フレームをキャプチャ取得
-    ret, frame = cap.read()
-    height, width, channels = frame.shape
-
-    # jpgに変換 画像ファイルをインターネットを介してAPIで送信するのでサイズを小さくしておく
-    small = cv2.resize(frame, (int(width * scale_factor), int(height * scale_factor)))
-    ret, buf = cv2.imencode('.jpg', small)
-
-    # Amazon RekognitionにAPIを投げる
-    faces = rekognition.detect_faces(Image={'Bytes':buf.tobytes()}, Attributes=['ALL'])
-
-    # 顔の周りに箱を描画する
-    for face in faces['FaceDetails']:
-        smile = face['Smile']['Value']
-        cv2.rectangle(frame,
-                      (int(face['BoundingBox']['Left']*width),
-                       int(face['BoundingBox']['Top']*height)),
-                      (int((face['BoundingBox']['Left']+face['BoundingBox']['Width'])*width),
-                       int((face['BoundingBox']['Top']+face['BoundingBox']['Height'])*height)),
-                      green if smile else red, frame_thickness)
-        emothions = face['Emotions']
-        i = 0
-        for emothion in emothions:
-            cv2.putText(frame,
-                        str(emothion['Type']) + ": " + str(emothion['Confidence']),
-                        (25, 40 + (i * 25)),
-                        fontface,
-                        fontscale,
-                        color)
-            i += 1
-
-    # 結果をディスプレイに表示
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+for face in response['FaceDetails']:
+    # 表情の分類結果を取得
+    emotions = face['Emotions']
+    
+    # 最も確率の高い表情を取得
+    max_emotion = max(emotions, key=lambda x: x['Confidence'])
+    emotion_name = max_emotion['Type']
+    emotion_confidence = max_emotion['Confidence']
+    
+    # 分類結果の表示
+    print('Emotion: {}, Confidence: {}'.format(emotion_name, emotion_confidence))

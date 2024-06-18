2f4b737a092d47842e41d78c6520c67946fe3500
HagiriMigita <starlight8620@gmail.com>
2024-06-18 13:10:19 +0900

    最新

--- test_4.py
+++ test_4.py
@@ -8,6 +8,8 @@ detector = Detector()
 
 cap = cv2.VideoCapture(0)
 
+i = 20
+
 def draw_fireworks(image, num_fireworks=5):
     for _ in range(num_fireworks):
         center = (np.random.randint(0, image.shape[1]), np.random.randint(0, image.shape[0]))
@@ -27,14 +29,17 @@ while cap.isOpened():
   # 入力画像をRGB形式に変換
   image.flags.writeable = False
   image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-  # 画像を一時ファイルに保存
-  with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as temp_image:
-      temp_image_path = temp_image.name
-      cv2.imwrite(temp_image_path, image)
-  # 入力画像をPy-Featに渡す
-  result = detector.detect_image(temp_image_path)
-  emotions = result.emotions.iloc[0]
-  print(emotions)
+
+  if i == 20:
+       # 画像を一時ファイルに保存
+      with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as temp_image:
+        temp_image_path = temp_image.name
+        cv2.imwrite(temp_image_path, image)
+      # 入力画像をPy-Featに渡す
+      result = detector.detect_image(temp_image_path)
+      emotions = result.emotions.iloc[0]
+      print(emotions)
+      i = 0
 
    # 表情が幸福感であればエフェクトを描画
   if 'happiness' in emotions and emotions['happiness'] > 0.5:  # 0.5は幸福感の閾値
@@ -45,6 +50,8 @@ while cap.isOpened():
   cv2.imshow('MediaPipe Face Mesh', cv2.flip(image, 1))
   end = time.perf_counter() #計測終了
   print('{:.2f}'.format((end-start)/60)) # 87.97(秒→分に直し、小数点以下の桁数を指定して出力)
+
+  i += 1
 
   # キー入力を待ち
   if cv2.waitKey(1) & 0xFF == 27:
